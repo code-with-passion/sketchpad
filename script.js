01bad71eeb1e57739c30a142a69c43c5668a8afe
code-with-passion <binovinoo@gmail.com>
2023-09-25 21:07:44 +0800

    Create a grid based on user prompt value given

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 const container = document.querySelector('.container');
-const square = document.createElement('div');
-square.classList.add('square');
 
-container.appendChild(square);
+function createGrid() {
+  // Clear any existing grid
+  container.innerHTML= '';
+
+  const gridContainer = document.createElement('div');
+  gridContainer.classList.add('grid-container');
+  container.appendChild(gridContainer);
+  
+  // Prompt the user for the grid size
+  const gridSize = parseInt(prompt("Enter the number of rows/columns for the grid:"));
+  
+  
+  // Set a CSS variable for grid size
+  document.documentElement.style.setProperty('--grid-size', gridSize);
+  
+  for (let i = 0; i < gridSize * gridSize; i++) {
+    const gridSquare = document.createElement('div');
+    gridSquare.classList.add('grid-square');
+    gridContainer.appendChild(gridSquare);
+  }
+}
+
+
